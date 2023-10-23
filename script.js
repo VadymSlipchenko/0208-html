1cf5da0ca66ee4791fb2cc99773e61b8ab337ed2
VadymSlipchenko <93735146+VadymSlipchenko@users.noreply.github.com>
2023-10-23 18:55:49 +0000

    removed unnecessary comments

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Initialize variables
+   
     let score = 0;
     let clickButton = document.getElementById("clickButton");
     let scoreDisplay = document.getElementById("score");
     let h1 = document.querySelector("h1");
 
-    // Function to update the score
+    
     function updateScore() {
         scoreDisplay.textContent = score;
 
-        // Check if the score is 1 and the text hasn't been changed yet
+       
         if (score === 1 && h1.textContent === "Clicker Game") {
             h1.textContent = "GO!!!";
         }
     }
 
-    // Function to animate the button on click
+
     function animateButtonClick() {
-        clickButton.style.backgroundColor = "#e74c3c"; // Change color on click
+        clickButton.style.backgroundColor = "#e74c3c"; 
         setTimeout(function () {
-            clickButton.style.backgroundColor = "#3498db"; // Reset color
+            clickButton.style.backgroundColor = "#3498db"; 
         }, 300);
     }
 
-    // Event listener for the button click
+    
     clickButton.addEventListener("click", function () {
         // Increase the score
         score++;
         updateScore();
 
-        // Animate the button on click
+       
         animateButtonClick();
     });
 
-    // Timer for the game (you can adjust the time limit)
-    const gameDuration = 20; // in seconds
+    
+    const gameDuration = 20; 
     let timeLeft = gameDuration;
 
     function updateTimer() {
         if (timeLeft <= 0) {
-            // Game over
+            
             clickButton.disabled = true;
         } else {
             timeLeft--;
@@ -47,9 +47,9 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    updateTimer(); // Start the timer
+    updateTimer(); 
 
-    // Display "Game Over" when the timer runs out
+    
     setTimeout(function () {
         clickButton.disabled = true;
         alert("Game Over! Your score: " + score);
